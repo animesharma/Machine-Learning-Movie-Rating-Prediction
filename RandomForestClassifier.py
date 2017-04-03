cc56d97873b8b5bf029468dd909012aef3d74145
Animesh Sharma <anime.mit5@gmail.com>
2017-04-03 11:26:53 +0530

    Update RandomForestClassifier.py

--- RandomForestClassifier.py
+++ RandomForestClassifier.py
@@ -6,12 +6,15 @@ from sklearn.metrics import accuracy_score
 
 
 # Fitting Random Forest Classification to the Training set
-classifier = RandomForestClassifier(n_estimators=999, criterion='entropy', random_state=0)
+classifier = RandomForestClassifier(n_estimators=999, criterion='entropy', random_state=0,
+                                    class_weight='balanced', n_jobs=-1)
 classifier.fit(X_train, Y_train.ravel())
 
 
 # Predicting the Test set results
 Y_pred = classifier.predict(X_test)
 Y_pred = Y_pred.reshape(-1,1)
-Accuracy = accuracy_score(Y_test, Y_pred, normalize=False, sample_weight=None)
+Accuracy = accuracy_score(Y_test, Y_pred, normalize=True, sample_weight=None)
+print 'Accuracy = ', Accuracy*100, '%'
+
 
