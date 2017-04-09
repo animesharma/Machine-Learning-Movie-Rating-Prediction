ee75a7d8252a3edb1751ec8a91343f20c0a5bbff
Sandeep Reddy <msande1996@gmail.com>
2017-04-09 19:06:25 +0530

    Minor cleanup. Phirbhi sab kuch meine hi kiya

--- RandomForestClassifier.py
+++ RandomForestClassifier.py
@@ -9,20 +9,6 @@ def print_full(x):
     print(x)
     pd.reset_option('display.max_rows')
 
-def round_off(x):
-    temp =float(x) - int(float(x))
-    if temp > 0.5 :
-        if temp > 0.75 :
-            temp = 1
-        else:
-            temp = 0.5
-    else:
-        if temp > 0.25 :
-            temp = 0.5
-        else :
-            temp = 0
-    return str(int(float(x)) + temp)
-
 def add(x,y):
     temp = float(x) + y
     return str(temp)
@@ -40,9 +26,6 @@ z1 = np.vectorize(add)
 ARRAYS_WITH_PREDS = []
 for i in np.arange(-0.5,0.6,0.1):
     ARRAYS_WITH_PREDS.append(z1(Y_pred,i))
-#Y_pred_1 = z1(Y_pred, 0.1)
-#Y_pred = mRounder(Y_pred)
-#Y_test = mRounder(Y_test)
 #print_full(np.c_[Y_pred,Y_test])
 final_accuracy = 0
 for preds in ARRAYS_WITH_PREDS:
