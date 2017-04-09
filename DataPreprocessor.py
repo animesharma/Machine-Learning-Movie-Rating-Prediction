b81ddc56fa9296c11535bd7a032d1394e2bcfa75
Sandeep Reddy <msande1996@gmail.com>
2017-04-09 23:51:12 +0530

    Consider new features

--- DataPreprocessor.py
+++ DataPreprocessor.py
@@ -25,11 +25,11 @@ country_pos = df.columns.get_loc("country")
 content_rating_pos = df.columns.get_loc("content_rating")
 
 #create a exclude list of these excluded attributes
-exlude_list = []
-exlude_list.append(director_name_pos)
-exlude_list.append(language_pos)
-exlude_list.append(country_pos)
-exlude_list.append(content_rating_pos)
+categorical_fts = []
+categorical_fts.append(director_name_pos)
+categorical_fts.append(language_pos)
+categorical_fts.append(country_pos)
+categorical_fts.append(content_rating_pos)
 
 #Array of features, exludes the last column
 X = df.iloc[:, :-1].values
@@ -40,27 +40,17 @@ Y = np.asarray(df.iloc[:, -1:].values, dtype="|S6") #numpy is moody
 label_director = LabelEncoder()
 X[0:,director_name_pos] = label_director.fit_transform(X[0:,director_name_pos])
 
-
-np.set_printoptions(threshold='nan')
-print(X)
-"""
-
-#Categorical Data Encoding
-l_lang = LabelEncoder()
-X[0:,6] = l_lang.fit_transform(X[0:, 6])
-l_count = LabelEncoder()
-X[0:,7] = l_count.fit_transform(X[0:, 7])
-
 # Missing Values
 # df = df.replace(np.nan, ' ', regex=True)  # Only works on dataframe object not on ndarray.
 imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
 X = imp.fit_transform(X)
 
 # Dummy Variable for CD
-o_h1 = OneHotEncoder(categorical_features=[6,7])
+o_h1 = OneHotEncoder(categorical_features=categorical_fts)
 X = o_h1.fit_transform(X).toarray()
 X = X[:,1:]
 #To account for Dummy Variable Trap; Removed First Column
+#TODO redo it with 0 considered
 
 # Splitting of Data
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.16, random_state = 0)
@@ -76,4 +66,3 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.16, rand
 # x_2d = tsne.fit_transform(X)
 # x_train_2d = tsne.fit_transform(X_train)
 # x_test_2d = tsne.fit_transform(X_test)
-"""
--- RandomForestClassifier.py
+++ RandomForestClassifier.py
@@ -4,17 +4,14 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-def print_full(x):
-    np.set_printoptions(threshold='nan')
-    print(x)
-
 def add(x,y):
     temp = float(x) + y
     return str(temp)
 
 # Fitting Random Forest Classification to the Training set
-classifier = RandomForestClassifier(n_estimators=999, criterion='entropy', random_state=0,
+classifier = RandomForestClassifier(n_estimators=99, criterion='entropy', random_state=0,
                                     class_weight='balanced', n_jobs=-1)
+                                    
 classifier.fit(X_train, Y_train.ravel())
 
 # Predicting the Test set results
@@ -24,7 +21,7 @@ z1 = np.vectorize(add)
 ARRAYS_WITH_PREDS = []
 for i in np.arange(-0.5,0.6,0.1):
     ARRAYS_WITH_PREDS.append(z1(Y_pred,i))
-#print_full(np.c_[Y_pred,Y_test])
+
 final_accuracy = 0
 for preds in ARRAYS_WITH_PREDS:
     final_accuracy+=accuracy_score(Y_test,preds,normalize=True,sample_weight=None)
