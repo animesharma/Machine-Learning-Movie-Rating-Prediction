0e7ad0380693044156cbf23f9502557a7a12a706
Sandeep Reddy <msande1996@gmail.com>
2017-04-10 18:19:35 +0530

    Feature Selection done

--- DataPreprocessor.py
+++ DataPreprocessor.py
@@ -7,65 +7,70 @@ from sklearn.preprocessing import (Imputer, LabelEncoder, OneHotEncoder,Standard
 from sklearn import metrics
 from sklearn.feature_selection import RFE
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
 
 # Importing Data
 dataset = pd.read_csv('Data.csv')
 
 # Removing some columns which aren't useful for our calculation
 df = dataset.drop([col for col in ['movie_title', 'color', 'plot_keywords', 'movie_imdb_link',
-                                   'aspect_ratio', 'genres','facenumber_in_poster']
+                                   'aspect_ratio', 'genres','actor_2_name','actor_3_name','actor_1_name',
+                                   'director_name']
                    if col in dataset], axis=1)
 
 #get the positions of the columns which are strings
-director_name_pos = df.columns.get_loc("director_name")
 language_pos = df.columns.get_loc("language")
 country_pos = df.columns.get_loc("country")
 content_rating_pos = df.columns.get_loc("content_rating")
-actor_1_name_pos = df.columns.get_loc("actor_1_name")
-actor_2_name_pos = df.columns.get_loc("actor_2_name")
-actor_3_name_pos = df.columns.get_loc("actor_3_name")
 
 
 #create a exclude list of these excluded attributes
 categorical_fts = []
-categorical_fts.append(director_name_pos)
 categorical_fts.append(language_pos)
 categorical_fts.append(country_pos)
 categorical_fts.append(content_rating_pos)
-categorical_fts.append(actor_1_name_pos)
-categorical_fts.append(actor_2_name_pos)
-categorical_fts.append(actor_3_name_pos)
 
 #Array of features, exludes the last column
 X = df.iloc[:, :-1].values
 
 # Last column array, string of length 6 (dtype)
 Ystr = np.asarray(df.iloc[:, df.shape[1]-1], dtype="|S6") #numpy is moody
+#Convert it to float
 Y = Ystr.astype(np.float)
 
-label_director = LabelEncoder()
-X[0:, director_name_pos] = label_director.fit_transform(X[0:, director_name_pos])
 label_language = LabelEncoder()
 X[0:, language_pos] = label_language.fit_transform(X[0:, language_pos])
 label_country = LabelEncoder()
 X[0:, country_pos] = label_country.fit_transform(X[0:, country_pos])
 label_content_rating = LabelEncoder()
 X[0:, content_rating_pos] = label_content_rating.fit_transform(X[0:, content_rating_pos])
-label_actor_1_name = LabelEncoder()
-X[0:, actor_1_name_pos] = label_actor_1_name.fit_transform(X[0:, actor_1_name_pos])
-label_actor_2_name = LabelEncoder()
-X[0:, actor_2_name_pos] = label_actor_2_name.fit_transform(X[0:, actor_2_name_pos])
-label_actor_3_name = LabelEncoder()
-X[0:, actor_3_name_pos] = label_actor_3_name.fit_transform(X[0:, actor_3_name_pos])
 
 imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
 X = imp.fit_transform(X)
 
+#Scaling X
+print(X[0:5,:])
+scaler = StandardScaler().fit(X)
+X = scaler.transform(X)
+print(X[0:5,:])
+
 #Lets pick the important features
 model = RandomForestClassifier()
-rfe = RFE(model,10)
+rfe = RFE(model,15)                  
 rfe = rfe.fit(X,Ystr)
-print(rfe.support_)
-print(rfe.ranking_)
-for imp in rfe.support_:
-    
+
+#print(rfe.support_)    #These are the coloumns we're keeping
+#print(rfe.ranking_)    #These are ranking the coloumns
+
+#Drop some of the coloumns. Keep 10 in the end
+drop_list = []
+for i in range(0,len(rfe.support_)):
+    if rfe.support_[i]:
+        print(df.columns.values[i])
+    else:
+        drop_list.append(i)
+X = np.delete(X,drop_list,axis=1)
+
+print(X.shape)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Ystr, test_size = 0.15, random_state = 0)
